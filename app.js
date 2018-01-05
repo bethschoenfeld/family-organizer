b1fe111a3e8766d04896c1b468905b53a32223c6
bethschoenfeld <bethingraham3@gmail.com>
2018-01-05 16:31:13 -0500

    finished my app.js

--- app.js
+++ app.js
@@ -1,55 +1,59 @@
 require('dotenv').config()
 
 const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI); 
+// view engine setup
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true
+})
 
 mongoose.connection.once('open', () => {
   console.log("Connected to MongoDB!!!")
 })
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+mongoose.connection.on('error', (error) => {
+  console.error(`
+  MongoDB connection error!
+  ${error}
+  `)
+  process.exit(-1)
+})
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const familyController = require('./controllers/familyController')
+app.use('/family', familyController)
 
-app.use('/', index);
-app.use('/users', users);
+const memberController = require('./controllers/memberController')
+app.use('/family/:familyId/members', membersController)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+const activityController = require('./controllers/activityController')
+app.use('/family/:familyId/members/:memberId/activity', activityController)
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/', (req,res) => {
+  response.redirect('/family')
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Express app listening on port ${PORT}`)
+})
 
 module.exports = app;
