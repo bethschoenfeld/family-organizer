53cfe12b1c861b9a4e4cf6168dc2571552277571
bethschoenfeld <bethingraham3@gmail.com>
2018-01-05 15:25:41 -0500

    finished the activity schema

--- db/schema.js
+++ db/schema.js
@@ -3,74 +3,16 @@ const Schema = mongoose.Schema
 
 mongoose.Promise = global.Promise
 
-const GiftSchema = new Schema(
+const ActivitySchema = new Schema(
     {
         title: {
             type: String,
-            required: [true, 'Gift title is required!']
+            required: [true, 'Need a title']
         },
         description: {
             type: String
         },
-        price: {
-            type: Number
-        },
-        cameFrom: {
+        location: {
             type: String
         }
-    },
-    {
-        timestamps: {}
-    }
 )
-
-const StoreSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Store name is required!']
-        },
-        address: {
-            type: String
-        },
-        giftsToReturn: [GiftSchema]
-    },
-    {
-        timestamps: {}
-    }
-)
-
-const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: [true, 'Username is required!']
-        },
-        email: {
-            type: String
-        },
-        firstName: {
-            type: String,
-            required: [true, 'First name is required!']
-        },
-        lastName: {
-            type: String,
-            required: [true, 'Last name is required!']
-        },
-        photoUrl: {
-            type: String,
-            default: 'https://cdn.vectorstock.com/i/thumb-large/66/69/santa-hat-vector-296669.jpg'
-        },
-        stores: [StoreSchema]
-    },
-    {
-        timestamps: {},
-        usePushEach: true
-    }
-)
-
-module.exports = {
-    UserSchema,
-    StoreSchema,
-    GiftSchema
-}
