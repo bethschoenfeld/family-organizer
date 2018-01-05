d5928d2baac6a4b516cdf15b8bd262ea24091584
bethschoenfeld <bethingraham3@gmail.com>
2018-01-05 14:47:31 -0500

    finished my seeded file.

--- db/seeds.js
+++ db/seeds.js
@@ -17,19 +17,42 @@ mongoose.connection.on('error', (error) => {
     process.exit(-1)
 })
 
-Family.remove({}).(() => {
+Family.remove({}).then(() => {
     const schoenfeld = new Family ({
         username: 'Schoenfeld',
         email: 'schoenfeld@family.com'
     })
     const tiberius = new Member({
         name: 'Tiberius the Fierce',
-        gender: 'male',
+        gender: 'male'
     })
     const chaseLaser = new Activity({
         title: 'Chase red dot',
         description: 'My nemisis the red dot needs to be taken out',
         location: 'My palace'
     })
+    tiberius.activities.push(chaseLaser)
+
+    const braille = new Member({
+        name: 'Braille',
+        gender: 'female'
+    })
+    const nap = new Activity({
+        title: 'Nap',
+        description: 'Nap in my favorite chair',
+        location: 'living Room'
+
+    })
+    braille.activities.push(nap)
+
+    schoenfeld.members.push(tiberius, braille)
+
+    return schoenfeld.save()
+}).catch((error) => {
+    console.log('Error saving seeded data!')
+    console.log(error)
+}).then(() => {
+    mongoose.connection.close()
+    console.log('Finished seeding database. Disconnected from MongoDB')
 })
 
